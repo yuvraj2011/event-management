0c05d37f1cce682216864b35f1ebd45d38ab7493
pythexcel <atul.etech2011@gmail.com>
2023-04-05 15:59:29 +0530

    completed

--- src/controllers/eventController.mjs
+++ src/controllers/eventController.mjs
@@ -1,8 +1,13 @@
-import { eventCreate, eventUpdate, findEvents } from "../services/eventServices.mjs";
+import {
+  deleteEvent,
+  eventCreate,
+  eventUpdate,
+  findEvents,
+} from "../services/eventServices.mjs";
 
-export const CreateEvent = async (req, res) => {
+const CreateEvent = async (req, res) => {
   const userId = req.user.id;
-  const data = await eventCreate({...req.body, userId});
+  const data = await eventCreate({ ...req.body, userId });
   if (!data.success) {
     const error = new Error(data.message);
     error.status = 500;
@@ -11,19 +16,31 @@ export const CreateEvent = async (req, res) => {
   res.status(201).json({ data });
 };
 
-const UpdateEvent = async(req,res) =>{
-    const userId = req.user.id;
-    const data = await eventUpdate({...req.body, userId});
-    if (!data.success) {
-        const error = new Error(data.message);
-        error.status = 500;
-        throw error;
-      }
-      res.status(200).json({ data });
-    };
-    export const GetEvents = async (req, res)=>{
-      const events = findEvents(req.params);
-      res.status(200).json(events)
-    }
+const UpdateEvent = async (req, res) => {
+  const userId = req.user.id;
+  const data = await eventUpdate({ ...req.body, userId });
+  if (!data.success) {
+    const error = new Error(data.message);
+    error.status = 500;
+    throw error;
+  }
+  res.status(200).json({ data });
+};
 
-export {CreateEvent, UpdateEvent};
+const GetEvents = async (req, res) => {
+  const events = await findEvents(req.query);
+  res.status(200).json(events);
+};
+
+const DeleteEvent = async (req, res) => {
+  const userId = req.user.id;
+  const event = await deleteEvent({ ...req.body, userId });
+  if (!event.success) {
+    const error = new Error(event.message);
+    error.status = 500;
+    throw error;
+  }
+  res.status(201).json({ event });
+};
+
+export { CreateEvent, UpdateEvent, GetEvents, DeleteEvent };
--- src/routes/event.mjs
+++ src/routes/event.mjs
@@ -1,6 +1,11 @@
 import express from "express";
 import errorHandler from "../helpers/errorHandler.mjs";
-import {CreateEvent, UpdateEvent, GetEvents} from "../controllers/eventController.mjs";
+import {
+  CreateEvent,
+  UpdateEvent,
+  GetEvents,
+  DeleteEvent,
+} from "../controllers/eventController.mjs";
 import { auth } from "../middlewares/middleware.mjs";
 
 const router = express.Router();
@@ -8,6 +13,9 @@ const router = express.Router();
 router.post("/create-event", auth, errorHandler(CreateEvent));
 
 router.post("/update-event", auth, errorHandler(UpdateEvent));
-router.get('/events', errorHandler(GetEvents))
-router.get('/events', errorHandler(GetEvents))
+
+router.get("/events", errorHandler(GetEvents));
+
+router.delete("/delete-event", auth, errorHandler(DeleteEvent));
+
 export default router;
